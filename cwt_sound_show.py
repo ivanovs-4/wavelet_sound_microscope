9f2a89e7a60966ba47f9c3aa5834293fccdb6cce
Sergey Ivanov <seiv@yandex-team.ru>
2014-06-05 22:09:56 +0600

    Fix showing

--- cwt_sound_show.py
+++ cwt_sound_show.py
@@ -10,22 +10,23 @@ import wavelet_continuous as wave
 
 bitrate, wav = wavfile.read('sample.wav')
 
-SIZE = 64 * 1
+SIZE = 64 * 2
 
 y = wav[:bitrate * SIZE / 64, 0]
 
 scales = wave.autoscales(N=y.shape[0], dt=100, dj=0.05, wf='morlet', p=2)
+
 compex_image = wave.cwt(y, dt=100, scales=scales, wf='morlet', p=2)
 
-# image = np.abs(compex_image)
-image = interpolation.zoom(np.abs(compex_image), (1, 1. / SIZE))
+abs_image = np.abs(compex_image)
+
+image = interpolation.zoom(abs_image, (1, 1. / SIZE))
 
 fig = plt.figure(1)
 
-ax = plt.subplot(2,1,1)
+ax = plt.subplot(1,1,1)
 p = ax.imshow(image)
 p.set_cmap('copper')
-# ax.autoscale_view(tight=True)
 
 fig.set_tight_layout(True)
 
