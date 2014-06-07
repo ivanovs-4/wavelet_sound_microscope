a17abb1ad16977dfa149ffb3171f3cac4016d654
Sergey Ivanov <seiv@yandex-team.ru>
2014-06-07 16:15:56 +0600

    Make n_samples as power of two. More than one second

--- check_wavelet_analyse.py
+++ check_wavelet_analyse.py
@@ -15,11 +15,17 @@ bitrate, stereo_wav = wavfile.read('sample.wav')
 
 wav = stereo_wav.transpose()[0]
 
-n_samples = bitrate
+# Make n_samples as power of two. More than one second
+n_samples = 2**(1 + int(np.log2(bitrate - 1)))
+
+print 'Bitrate:', bitrate
+print 'N samples:', n_samples
 
 wbox = WaveletBox(N=n_samples, dt=1, dj=1/12.)
 
 n_images = (wav.shape[0] - 1) / n_samples + 1
+print 'N images:', n_images
+
 padded_wav_size = n_images * n_samples
 
 wav_pieces = np.pad(wav, (0, padded_wav_size - wav.shape[0]), 'constant') \
